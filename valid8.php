20f9fd8015a1e77a465697de140359a4e3799e33
methodin <derekrw@gmail.com>
2011-02-01 20:38:35 -0500

    Message modification, additional checks

--- valid8.php
+++ valid8.php
@@ -43,43 +43,43 @@ class Valid8 {
 				case 'int':
 					if(!is_int($this->params[$this->key]))
 					{
-						$this->fatal("{$this->key} is not a valid integer");
+						$this->fatal("{$this->key} is not a valid integer.");
 					}
 					break;
 				case 'numeric':
 					if(!is_numeric($this->params[$this->key]))
 					{
-						$this->fatal("{$this->key} is not a valid number");
+						$this->fatal("{$this->key} is not a valid number.");
 					}
 					break;
 				case 'boolean':
 					if(!is_bool($this->params[$this->key]))
 					{
-						$this->fatal("{$this->key} is not a valid boolean");
+						$this->fatal("{$this->key} is not a valid boolean.");
 					}
 					break;
 				case 'array':
 					if(!is_array($this->params[$this->key]))
 					{
-						$this->fatal("{$this->key} is not a valid array");
+						$this->fatal("{$this->key} is not a valid array.");
 					}
 					break;
 				case 'date':
 					if(!is_string($this->params[$this->key]) || !preg_match('/((19|20)\\d\\d)\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])/', $this->params[$this->key]))
 					{
-						$this->fatal("{$this->key} is not a valid date");
+						$this->fatal("{$this->key} is not a valid date.");
 					}
 					break;
 				case 'datetime':
 					if(!is_string($this->params[$this->key]) || !preg_match('/((19|20)\\d\\d)\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01]) ([0][0-9]|[1][0-9]|[2][0-3]):([0-5][0-9]):([0-5][0-9])/', $this->params[$this->key]))
 					{
-						$this->fatal("{$this->key} is not a valid datetime");
+						$this->fatal("{$this->key} is not a valid datetime.");
 					}
 					break;
 				case 'string':
 					if(!is_string($this->params[$this->key]))
 					{
-						$this->fatal("{$this->key} is not a valid string");
+						$this->fatal("{$this->key} is not a valid string.");
 					}
 					break;
 				default:
@@ -95,7 +95,7 @@ class Valid8 {
 	{
 		if(!$this->fatal && !isset($this->params[$this->key]))
 		{
-			$this->error("{$this->key} is a required parameter");
+			$this->error("{$this->key} is a required parameter.");
 		}
 		return $this;
 	}
@@ -104,7 +104,7 @@ class Valid8 {
 	{
 		if(!$this->fatal && isset($this->params[$this->key]) && empty($this->params[$this->key]))
 		{
-			$this->error("{$this->key} in parameter list cannot be empty");
+			$this->error("{$this->key} in parameter list cannot be empty.");
 		}
 		return $this;
 	}
@@ -113,7 +113,7 @@ class Valid8 {
 	{
 		if(!$this->fatal && isset($this->params[$this->key]) && $this->params[$this->key] <= $value)
 		{
-			$this->error("{$this->key} value of [{$this->params[$this->key]}] is not greater than [{$value}]");
+			$this->error("{$this->key} value of [{$this->params[$this->key]}] is not greater than [{$value}].");
 		}
 		return $this;
 	}
@@ -122,7 +122,25 @@ class Valid8 {
 	{
 		if(!$this->fatal && isset($this->params[$this->key]) && $this->params[$this->key] >= $value)
 		{
-			$this->error("{$this->key} value of [{$this->params[$this->key]}] is not less than [{$value}]");
+			$this->error("{$this->key} value of [{$this->params[$this->key]}] is not less than [{$value}].");
+		}
+		return $this;
+	}
+
+	function length($value)
+	{
+		if(!$this->fatal && $this->type == 'string' && strlen($this->params[$this->key]) > $value)
+		{
+			$this->error("{$this->key} must be less than {$value} characters.");
+		}
+		return $this;
+	}
+
+	function is_not($value)
+	{
+		if(!$this->fatal && $this->params[$this->key] === $value)
+		{
+			$this->error("{$this->key} must not be [{$value}].");
 		}
 		return $this;
 	}
@@ -153,7 +171,7 @@ class Valid8 {
 			}
 			if(!preg_match($pattern, $this->params[$this->key]))
 			{
-				$this->error("{$this->key} does not match exepected pattern");
+				$this->error("{$this->key} does not match exepected pattern.");
 			}
 		}
 		return $this;
@@ -168,16 +186,16 @@ class Valid8 {
 			{
 				if($this->type != 'array' && array_search($this->params[$this->key], $value) === false)
 				{
-					$this->error("{$this->key} contains an unexpected value");
+					$this->error("{$this->key} contains an unexpected value.");
 				}
 				else if($this->type == 'array' && count(array_intersect($this->params[$this->key], $value)) != count($this->params[$this->key]))
 				{
-					$this->error("{$this->key} contains an unexpected value");
+					$this->error("{$this->key} contains an unexpected value.");
 				}
 			}
 			else
 			{
-				$this->error("Expected an array with at least one value for the \"in\" comparison against {$this->key}");
+				$this->error("Expected an array with at least one value for the \"in\" comparison against {$this->key}.");
 			}
 		}
 		return $this;
@@ -189,7 +207,7 @@ class Valid8 {
 		if(count($diff))
 		{
 			$keys = implode(', ',$diff);
-			$this->error("The following parameters are not valid for this function: {$keys}");
+			$this->error("The following parameters are not valid for this function: {$keys}.");
 		}
 		if(count($this->errors) > 0)
 		{
