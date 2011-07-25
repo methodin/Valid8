cf23525bea3ed662e1a23c52bb2cc643bf28de4c
methodin <derekrw@gmail.com>
2011-07-25 19:29:28 -0400

    General usability changes, a couple new functions

--- valid8.php
+++ valid8.php
@@ -1,12 +1,14 @@
 <?php
-class Valid8 {
-	private $response = array();
-	private $params = array();
-	private $key = false;
-	private $errors = array();
-	private $type = false;
-	private $fatal = false;
-	private $key_list = array();
+class Validate {
+	private $response = array(); // Generic function response
+	private $params = array(); // Incoming parameter array
+	private $key = false; // Current key in params being checked
+	private $fancy = false; // String representation of the key
+	private $errors = array(); // Error buffer
+	private $type = false; // Type of currenty $params[$key] element
+	private $fatal = false; // We've failed - misereably
+	private $key_list = array(); // Buffer to make sure no unexpected parameters are passed
+	private $message = false; // Override default error message
 
 	function __construct(&$r, &$p)
 	{
@@ -14,24 +16,51 @@ class Valid8 {
 		$this->params =& $p;
 	}
 
+	// General validation error message
 	private function error($msg)
 	{
-		$this->errors[] = $msg;
+		$this->errors[] = $this->message ? $this->message : $msg;
 	}
 
+	// Fatal errored - for a particular key is a string when expecting an int
+	// No sense in proceeding with additional checked
 	private function fatal($msg)
 	{
 		$this->fatal = true;
 		$this->errors[] = $msg;
 	}
 
+	// Ignore a particular incoming key
+	function ignore($key)
+	{
+		$this->key_list[] = $key;
+		return $this;
+	}	
+
+	// Start checking $key of $params array
 	function check($key)
 	{
 		$this->key_list[] = $key;
 		$this->key = $key;
+		$this->fancy = ucwords(str_replace(array('_', '-', '+'), ' ', strtolower($key)));
+		$this->message = false;
 		return $this;
 	}
 
+	// Set a message to use on the next error
+	function message($msg)
+	{
+		$this->message = $msg;
+		return $this;
+	}
+
+	function fancy($name)
+	{
+		$this->fancy = $name;
+		return $this;
+	}
+
+	// Time for type validation
 	function is($type)
 	{
 		$this->fatal = false;
@@ -43,43 +72,43 @@ class Valid8 {
 				case 'int':
 					if(!is_int($this->params[$this->key]))
 					{
-						$this->fatal("{$this->key} is not a valid integer.");
+						$this->fatal("{$this->fancy} is not a valid integer.");
 					}
 					break;
 				case 'numeric':
 					if(!is_numeric($this->params[$this->key]))
 					{
-						$this->fatal("{$this->key} is not a valid number.");
+						$this->fatal("{$this->fancy} is not a valid number.");
 					}
 					break;
 				case 'boolean':
 					if(!is_bool($this->params[$this->key]))
 					{
-						$this->fatal("{$this->key} is not a valid boolean.");
+						$this->fatal("{$this->fancy} is not a valid boolean.");
 					}
 					break;
 				case 'array':
 					if(!is_array($this->params[$this->key]))
 					{
-						$this->fatal("{$this->key} is not a valid array.");
+						$this->fatal("{$this->fancy} is not a valid array.");
 					}
 					break;
 				case 'date':
 					if(!is_string($this->params[$this->key]) || !preg_match('/((19|20)\\d\\d)\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])/', $this->params[$this->key]))
 					{
-						$this->fatal("{$this->key} is not a valid date.");
+						$this->fatal("{$this->fancy} is not a valid date.");
 					}
 					break;
 				case 'datetime':
 					if(!is_string($this->params[$this->key]) || !preg_match('/((19|20)\\d\\d)\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01]) ([0][0-9]|[1][0-9]|[2][0-3]):([0-5][0-9]):([0-5][0-9])/', $this->params[$this->key]))
 					{
-						$this->fatal("{$this->key} is not a valid datetime.");
+						$this->fatal("{$this->fancy} is not a valid datetime.");
 					}
 					break;
 				case 'string':
 					if(!is_string($this->params[$this->key]))
 					{
-						$this->fatal("{$this->key} is not a valid string.");
+						$this->fatal("{$this->fancy} is not a valid string.");
 					}
 					break;
 				default:
@@ -88,72 +117,110 @@ class Valid8 {
 					break;
 			}
 		}
+		$this->message = false;
 		return $this;
 	}
 
-	function required()
+	// See if the value is equal to the parameter
+	function equal($value)
 	{
-		if(!$this->fatal && !isset($this->params[$this->key]))
+		if(!$this->fatal && $this->params[$this->key] != $value)
 		{
-			$this->error("{$this->key} is a required parameter.");
+			$this->error("{$this->key} is not the expected value.");
 		}
+		$this->message = false;
 		return $this;
-	}
+	}	
 
-	function not_empty()
+	// Check that the current value being checked was passed
+	function required($condition=true)
 	{
-		if(!$this->fatal && isset($this->params[$this->key]) && empty($this->params[$this->key]))
+		if(!$this->fatal && !isset($this->params[$this->key]) && $condition)
 		{
-			$this->error("{$this->key} in parameter list cannot be empty.");
+			$this->error("{$this->fancy} is a required parameter.");
 		}
+		$this->message = false;
 		return $this;
 	}
 
+	// Check that the value is not empty (literal empty)
+	function not_empty($condition=true)
+	{
+		if(!$this->fatal && $condition && isset($this->params[$this->key]) && empty($this->params[$this->key]))
+		{
+			$this->error("{$this->fancy} must not be empty.");
+		}
+		$this->message = false;
+		return $this;
+	}
+
+	// Compare the value against a minimum
 	function greater_than($value)
 	{
 		if(!$this->fatal && isset($this->params[$this->key]) && $this->params[$this->key] <= $value)
 		{
-			$this->error("{$this->key} value of [{$this->params[$this->key]}] is not greater than [{$value}].");
+			$this->error("{$this->fancy} value of [{$this->params[$this->key]}] is not greater than [{$value}].");
 		}
+		$this->message = false;
 		return $this;
 	}
 
+	// Compare the value against a maximum
 	function less_than($value)
 	{
 		if(!$this->fatal && isset($this->params[$this->key]) && $this->params[$this->key] >= $value)
 		{
-			$this->error("{$this->key} value of [{$this->params[$this->key]}] is not less than [{$value}].");
+			$this->error("{$this->fancy} value of [{$this->params[$this->key]}] is not less than [{$value}].");
 		}
+		$this->message = false;
 		return $this;
 	}
 
-	function length($value)
+	// Maximum length of a string
+	function max_length($value)
 	{
 		if(!$this->fatal && $this->type == 'string' && strlen($this->params[$this->key]) > $value)
 		{
-			$this->error("{$this->key} must be less than {$value} characters.");
+			$this->error("{$this->fancy} must be fewer than {$value} characters.");
 		}
+		$this->message = false;
+		return $this;
+	}
+	
+	// Minimum length of a string
+	function min_length($value)
+	{
+		if(!$this->fatal && $this->type == 'string' && strlen($this->params[$this->key]) < $value)
+		{
+			$this->error("{$this->fancy} must be at least {$value} characters.");
+		}
+		$this->message = false;
 		return $this;
 	}
 
+	// Don't accept a particular value
 	function is_not($value)
 	{
 		if(!$this->fatal && $this->params[$this->key] === $value)
 		{
-			$this->error("{$this->key} must not be [{$value}].");
+			$this->error("{$this->fancy} must not be [{$value}].");
 		}
+		$this->message = false;
 		return $this;
 	}
 
+	// If not passed at all set to a default
 	function default_to($value)
 	{
 		if(!$this->fatal && !isset($this->params[$this->key]))
 		{
 			$this->params[$this->key] = $value;
 		}
+		$this->message = false;
 		return $this;
 	}
 
+	// Run a preset or custom regex pattern
 	function regex($value)
 	{
 		if(isset($this->params[$this->key]))
@@ -174,12 +241,15 @@ class Valid8 {
 			}
 			if(!preg_match($pattern, $this->params[$this->key]))
 			{
-				$this->error("{$this->key} does not match exepected pattern.");
+				$this->error("{$this->fancy} does not match expected pattern.");
 			}
 		}
+		$this->message = false;
 		return $this;
 	}
 
+	// Only allow the value to be in a certain set of values
+	// Passed as in(value1, value2, value3)
 	function in()
 	{
 		if(!$this->fatal)
@@ -189,21 +259,23 @@ class Valid8 {
 			{
 				if($this->type != 'array' && array_search($this->params[$this->key], $value) === false)
 				{
-					$this->error("{$this->key} contains an unexpected value.");
+					$this->error("{$this->fancy} contains an unexpected value.");
 				}
 				else if($this->type == 'array' && count(array_intersect($this->params[$this->key], $value)) != count($this->params[$this->key]))
 				{
-					$this->error("{$this->key} contains an unexpected value.");
+					$this->error("{$this->fancy} contains an unexpected value.");
 				}
 			}
 			else
 			{
-				$this->error("Expected an array with at least one value for the \"in\" comparison against {$this->key}.");
+				$this->error("Expected an array with at least one value for the \"in\" comparison against {$this->fancy}.");
 			}
 		}
+		$this->message = false;
 		return $this;
 	}
 
+	// Perform validation
 	function run()
 	{
 		$diff = array_diff(array_keys($this->params), $this->key_list);
@@ -222,16 +294,17 @@ class Valid8 {
 	}
 }
 
-function valid8(&$response, &$params)
+function validate(&$response, &$params)
 {
 	if(!isset($response))
 	{
 		$response = array();
 	}
+	// Set response to a default value
 	$response = array_merge($response,array(
 		'status' => 1,
 		'message' => array(),
 		'result' => array()
 	));
-	return new Valid8($response, $params);
+	return new Validate($response, $params);
 }
